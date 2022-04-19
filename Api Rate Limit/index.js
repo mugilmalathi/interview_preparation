f6b70825c9230278450ee5fb82c61cf63e31e7e7
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-04-19 16:04:09 +0530

    api rate limit done with maximum limit 5 and 10sec

---
+++ Api Rate Limit/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const datas = require("./data");
+const ratelimit = require("express-rate-limit");
+
+const limiter = ratelimit({
+    max: 5,
+    windowMs: 10000,
+})
+
+app.get("/data", limiter, (req, res)=>{
+    try {
+        res.send({
+            status: "Success",
+            data: datas
+        })
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+app.listen(1996, ()=>{
+    console.log("Listening on Port 1996");
+})
